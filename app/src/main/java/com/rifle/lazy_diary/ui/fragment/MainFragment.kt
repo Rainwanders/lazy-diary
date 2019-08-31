949b0aca1de491bc0a69f4044f8f0bc191a73435
RainWander <414722462@qq.com>
2019-08-31 20:51:59 +0800

    feat: 190831_modify_mood_spirit_selector
    完善心情，精神状态选择；

--- app/src/main/java/com/rifle/lazy_diary/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/rifle/lazy_diary/ui/fragment/MainFragment.kt
@@ -1,32 +1,91 @@
 package com.rifle.lazy_diary.ui.fragment
 
-import android.animation.ObjectAnimator
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.FrameLayout
-import android.widget.Toast
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.rifle.lazy_diary.R
-import com.rifle.lazy_diary.ui.base.BaseFragment
-import kotlinx.android.synthetic.main.fragment_main.*
+import com.rifle.lazy_diary.databinding.FragmentMainBinding
+import com.rifle.lazy_diary.widgets.StatusSelector
+import yalantis.com.sidemenu.interfaces.ScreenShotable
 
-class MainFragment : BaseFragment() {
+class MainFragment : Fragment(), ScreenShotable {
+    private lateinit var mBinding: FragmentMainBinding
+    private lateinit var mMoodList: ArrayList<Int>
+    private lateinit var mSpiritList: ArrayList<Int>
 
-    private var mMindMargin = 0
-    private lateinit var mMindParam: FrameLayout.LayoutParams
 
-    override fun setLayout(): Int {
-        return R.layout.fragment_main
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        mBinding = DataBindingUtil.inflate(inflater,
+                R.layout.fragment_main, container, false)
+        return mBinding.root
     }
 
-    override fun initView() {
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
     }
 
-//    private fun initAnim() {
-//        mMindParam = imageView.layoutParams as FrameLayout.LayoutParams
-//        val mindAnim = ObjectAnimator.ofFloat(imageView, "translationX", 0f, 200f)
-//        mindAnim.duration = 300
-//        imageView.setOnClickListener {
-//            mindAnim.start()
-//        }
-//    }
+
+    private fun initView() {
+        initStatusList()
+        setClickEvent()
+        setSelectListener()
+        mBinding.moodSelector.setImageList(mMoodList)
+        mBinding.spiritSelector.setImageList(mSpiritList)
+    }
+
+
+    private fun initStatusList() {
+        mMoodList = arrayListOf(
+                R.drawable.ic_mood_normal,
+                R.drawable.ic_mood_happy,
+                R.drawable.ic_mood_angry,
+                R.drawable.ic_mood_sad,
+                R.drawable.ic_mood_cry
+        )
+
+        mSpiritList = arrayListOf(
+                R.drawable.ic_spirit_boring,
+                R.drawable.ic_spirit_cute,
+                R.drawable.ic_spirit_fighting,
+                R.drawable.ic_spirit_faint,
+                R.drawable.ic_spirit_sleepy
+        )
+    }
+
+    private fun setClickEvent() {
+        mBinding.currentMood.setOnClickListener {
+            mBinding.moodSelector.startExpandAnim()
+        }
+
+        mBinding.currentSpirit.setOnClickListener {
+            mBinding.spiritSelector.startExpandAnim()
+        }
+    }
+
+
+    private fun setSelectListener() {
+        mBinding.moodSelector.setSelectListener(
+                object : StatusSelector.OnSelectListener {
+                    override fun select(index: Int) {
+                        mBinding.currentMood.setImageResource(mMoodList[index])
+                    }
+                })
+
+        mBinding.spiritSelector.setSelectListener(
+                object : StatusSelector.OnSelectListener {
+                    override fun select(index: Int) {
+                        mBinding.currentSpirit.setImageResource(mSpiritList[index])
+                    }
+                })
+    }
+
+    override fun getBitmap(): Bitmap? = null
+    override fun takeScreenShot() {}
 }
--- app/src/main/java/com/rifle/lazy_diary/widgets/StatusSelector.kt
+++ app/src/main/java/com/rifle/lazy_diary/widgets/StatusSelector.kt
@@ -1,21 +1,58 @@
 package com.rifle.lazy_diary.widgets
 
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import com.rifle.lazy_diary.R
 
 class StatusSelector : ViewGroup {
 
     private lateinit var mContext: Context
 
-    private var mWidth: Int = 0
-    private var mHeight: Int = 0
+    private var mWidth = 0
+    private var mHeight = 0
 
-    private var mIndicatorPoint = 0
+    /**
+     * 图片尺寸大小
+     */
+    private var mImageSize = 0
 
-    private val mPositionList: List<IntArray> = ArrayList()
+    /**
+     * 图片尺寸加上图片间尺寸
+     */
+    private var mWithMarginSize = 0
+
+    /**
+     * 属性动画的标尺
+     */
+    private var mIndicator = 0f
+
+    /**
+     * 是否已经添加了子View图片
+     */
+    private var mIsViewAdded = false
+
+    /**
+     * 展开属性动画
+     */
+    private var mAnimator: ObjectAnimator? = null
+
+    /**
+     * 图片间间隔尺寸
+     */
+    private val mInterval = 20
+
+    /**
+     * 图片list
+     */
+    private lateinit var mImageList: ArrayList<Int>
+
+    /**
+     * 选择图片监听
+     */
+    private var mSelectListener: OnSelectListener? = null
 
     constructor(context: Context) : super(context) {
         init(context)
@@ -35,32 +72,117 @@ class StatusSelector : ViewGroup {
         mContext = context
     }
 
-    private fun addImage() {
-        val imageView = ImageView(mContext)
-        imageView.setImageResource(R.drawable.ic_mood_normal)
-        this.addView(imageView)
-        val layoutParams = imageView.layoutParams
-        layoutParams.width = mHeight
-        layoutParams.height = mHeight
-    }
-
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
-        mWidth = w
-        mHeight = h
-        addImage()
-    }
-
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        mWidth = getFinalSize(300, widthMeasureSpec)
+        mHeight = getFinalSize(100, heightMeasureSpec)
 
+        mImageSize = (mWidth - 4 * mInterval) / 5
+        mHeight = mImageSize
+        mWithMarginSize = mImageSize + mInterval
+        setMeasuredDimension(mWidth, mHeight)
     }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
         for (i in 0 until childCount) {
-            getChildAt(i).layout(0, 0, 0, 0)
+            val left = (mIndicator * mWithMarginSize * i).toInt()
+            val right = mImageSize + left
+            getChildAt(i).layout(left, 0, right, mHeight)
         }
     }
 
+
+    /**
+     * 懒加载动画
+     */
+    private fun initAnimator() {
+        if (mAnimator == null) {
+            mAnimator = ObjectAnimator.ofFloat(this, "indicator", 0f)
+            mAnimator?.duration = 500
+        }
+    }
+
+
+    /**
+     * 启动展开动画
+     */
+    fun startExpandAnim() {
+        if (!mIsViewAdded) {
+            addImages()
+        }
+        initAnimator()
+        mAnimator?.setFloatValues(0f, 1f)
+        mAnimator?.start()
+        visibility = View.VISIBLE
+    }
+
+
+    /**
+     * 设置选择监听
+     */
+    fun setSelectListener(mListener: OnSelectListener) {
+        mSelectListener = mListener
+    }
+
+
+    /**
+     * 添加图片
+     */
+    private fun addImages() {
+        mIsViewAdded = true
+        mImageList.forEachIndexed { index, image ->
+            val imageView = ImageView(mContext)
+            imageView.setImageResource(image)
+            this.addView(imageView)
+            val params = imageView.layoutParams
+            params.width = mImageSize
+            params.height = mImageSize
+            imageView.layoutParams = params
+
+            imageView.setOnClickListener {
+                initAnimator()
+                mSelectListener?.select(index)
+                mAnimator?.setFloatValues(1f, 0f)
+                mAnimator?.start()
+                visibility = View.GONE
+            }
+        }
+    }
+
+
+    /**
+     * 获取最终的尺寸
+     */
+    private fun getFinalSize(defaults: Int, measureSpec: Int): Int {
+        val mode = MeasureSpec.getMode(measureSpec)
+        val size = MeasureSpec.getSize(measureSpec)
+        return if (mode == MeasureSpec.EXACTLY) size else defaults
+    }
+
+
+    /**
+     * 添加图片list
+     */
+    fun setImageList(imageList: List<Int>) {
+        this.mImageList = imageList as ArrayList<Int>
+    }
+
+
+    /**
+     * 属性动画标尺
+     */
+    private fun setIndicator(indicator: Float) {
+        this.mIndicator = indicator
+        this.alpha = indicator
+        requestLayout()
+    }
+
+
+    /**
+     * 选择监听回调接口
+     */
+    interface OnSelectListener {
+        fun select(index: Int)
+    }
 }
